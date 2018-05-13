51ca3d8ae4a053d1f35f39acbb32f50621f56830
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-13 16:00:07 -0400

    connection

---
+++ models/Burger.js
@@ -0,0 +1,15 @@
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js")
+
+var Burger = sequelize.define("burgers", {
+    burger_name: Sequelize.STRING,
+    devoured: Sequelize.BOOLEAN}, {    
+}, {
+    timestamps: false
+});
+
+Burger.sync();
+
+module.exports = Burger;
+
+ 
